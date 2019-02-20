9f6d2d4d0365d2b61a50b77edf6bc9c54e5aff54
dhoule <caine2003@Daniels-MacBook-Air.local>
2019-02-20 00:52:10 -0600

    Changed to make data normalized, somewhat
    Gets rid of NaN values given by identity function.

--- runIrisData.m
+++ runIrisData.m
@@ -1,29 +1,35 @@
 % This file was used for question 3 of HW 1. It has been modified from start to finish. This is the final form.
 
-epochs = 100;
-eta = 0.1;
+epochs = 10000;
+eta = 0.15;
 
 data = importdata("iris_clone.data"); % The data file must be in the same directory
 
+% TODO need to split into 2 sets.
+  % Last 10 rows needs to be the test set for each of these
 first = data(1:50,:);
 second = data(51:100,:);
 third = data(101:150,:);
 
+temp1p = [first(1:40,:); second(1:40,:); first(41:50,:); second(41:50,:)];
+temp1 = [norm_sigma(temp1p(:,1)) norm_sigma(temp1p(:,2)) norm_sigma(temp1p(:,3)) norm_sigma(temp1p(:,4))];
+temp2p = [second(1:40,:); third(1:40,:); second(41:50,:); third(41:50,:)];
+temp2 = [norm_sigma(temp2p(:,1)) norm_sigma(temp2p(:,2)) norm_sigma(temp2p(:,3)) norm_sigma(temp2p(:,4))];
+temp3p = [first(1:40,:); third(1:40,:); first(41:50,:); third(41:50,:)];
+temp3 = [norm_sigma(temp3p(:,1)) norm_sigma(temp3p(:,2)) norm_sigma(temp3p(:,3)) norm_sigma(temp3p(:,4))];
+yValues = [(-1 * ones(40,1));  ones(40,1); (-1 * ones(10,1));  ones(10,1)];
 
-temp1 = [first; second];
-temp2 = [second; third];
-temp3 = [first; third];
-yValues = [(-1 * ones(50,1));  ones(50,1)];
 
 % Run of the sepal data
 printf("First & Second Sepal ------------\n")
 % sepalLearned1 = RunPLA(epochs, temp1(:,1:2), yValues);
-% sepalLearned1 = RunDLA_ident(eta, epochs, temp1(:,1:2), yValues);
-% sepalLearned1 = RunDLA_sigmoid(eta, epochs, temp1(:,1:2), yValues);
-sepalLearned1 = RunDLA_tanh(eta, epochs, temp1(:,1:2), yValues);
+% sepalLearned1 = RunDLA_ident(eta, epochs, temp1(1:80,1:2), yValues(1:80,1));
+% sepalLearned1 = RunDLA_sigmoid(eta, epochs, temp1(1:80,1:2), yValues(1:80,1));
+sepalLearned1 = RunDLA_tanh(eta, epochs, temp1(1:80,1:2), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(sepalLearned1)
-sepalwnorm1 = wnormalize(sepalLearned1);
+% sepalwnorm1 = wnormalize(sepalLearned1);
+sepalwnorm1 = norm_sigma(sepalLearned1);
 printf("Normalized weight vector: ")
 disp(sepalwnorm1)
 printf("Figure 1\n")
@@ -33,12 +39,13 @@ PlotIrisData(sepalwnorm1, temp1(:,1:2), yValues)
 printf("---------------------------\n")
 printf("Second & Third Sepal ------------\n")
 % sepalLearned2 = RunPLA(epochs, temp2(:,1:2), yValues);
-% sepalLearned2 = RunDLA_ident(eta, epochs, temp2(:,1:2), yValues);
-% sepalLearned2 = RunDLA_sigmoid(eta, epochs, temp2(:,1:2), yValues);
-sepalLearned2 = RunDLA_tanh(eta, epochs, temp2(:,1:2), yValues);
+% sepalLearned2 = RunDLA_ident(eta, epochs, temp2(1:80,1:2), yValues(1:80,1));
+% sepalLearned2 = RunDLA_sigmoid(eta, epochs, temp2(1:80,1:2), yValues(1:80,1));
+sepalLearned2 = RunDLA_tanh(eta, epochs, temp2(1:80,1:2), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(sepalLearned2)
-sepalwnorm2 = wnormalize(sepalLearned2);
+% sepalwnorm2 = wnormalize(sepalLearned2);
+sepalwnorm2 = norm_sigma(sepalLearned2);
 printf("Normalized weight vector: ")
 disp(sepalwnorm2)
 printf("Figure 2\n")
@@ -48,12 +55,13 @@ PlotIrisData(sepalwnorm2, temp2(:,1:2), yValues)
 printf("---------------------------\n")
 printf("First & Third Sepal -------------\n")
 % sepalLearned3 = RunPLA(epochs, temp3(:,1:2), yValues);
-% sepalLearned3 = RunDLA_ident(eta, epochs, temp3(:,1:2), yValues);
-% sepalLearned3 = RunDLA_sigmoid(eta, epochs, temp3(:,1:2), yValues);
-sepalLearned3 = RunDLA_tanh(eta, epochs, temp3(:,1:2), yValues);
+% sepalLearned3 = RunDLA_ident(eta, epochs, temp3(1:80,1:2), yValues(1:80,1));
+% sepalLearned3 = RunDLA_sigmoid(eta, epochs, temp3(1:80,1:2), yValues(1:80,1));
+sepalLearned3 = RunDLA_tanh(eta, epochs, temp3(1:80,1:2), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(sepalLearned3)
-sepalwnorm3 = wnormalize(sepalLearned3);
+% sepalwnorm3 = wnormalize(sepalLearned3);
+sepalwnorm3 = norm_sigma(sepalLearned3);
 printf("Normalized weight vector: ")
 disp(sepalwnorm3)
 printf("Figure 3\n")
@@ -70,12 +78,13 @@ printf("---------------------------\n\n\n")
 printf("First & Second Petal ------------\n")
 % Run of the petal data
 % petalLearned1 = RunPLA(epochs, temp1(:,3:4), yValues);
-% petalLearned1 = RunDLA_ident(eta, epochs, temp1(:,3:4), yValues);
-% petalLearned1 = RunDLA_sigmoid(eta, epochs, temp1(:,3:4), yValues);
-petalLearned1 = RunDLA_tanh(eta, epochs, temp1(:,3:4), yValues);
+% petalLearned1 = RunDLA_ident(eta, epochs, temp1(1:80,3:4), yValues(1:80,1));
+% petalLearned1 = RunDLA_sigmoid(eta, epochs, temp1(1:80,3:4), yValues(1:80,1));
+petalLearned1 = RunDLA_tanh(eta, epochs, temp1(1:80,3:4), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(petalLearned1)
-petalwnorm1 = wnormalize(petalLearned1);
+% petalwnorm1 = wnormalize(petalLearned1);
+petalwnorm1 = norm_sigma(petalLearned1);
 printf("Normalized weight vector: ")
 disp(petalwnorm1)
 printf("Figure 4\n")
@@ -85,12 +94,13 @@ PlotIrisData(petalwnorm1, temp1(:,3:4), yValues)
 printf("---------------------------\n")
 printf("Second & Third Petal ------------\n")
 % petalLearned2 = RunPLA(epochs, temp2(:,3:4), yValues);
-% petalLearned2 = RunDLA_ident(eta, epochs, temp2(:,3:4), yValues);
-% petalLearned2 = RunDLA_sigmoid(eta, epochs, temp2(:,3:4), yValues);
-petalLearned2 = RunDLA_tanh(eta, epochs, temp2(:,3:4), yValues);
+% petalLearned2 = RunDLA_ident(eta, epochs, temp2(1:80,3:4), yValues(1:80,1));
+% petalLearned2 = RunDLA_sigmoid(eta, epochs, temp2(1:80,3:4), yValues(1:80,1));
+petalLearned2 = RunDLA_tanh(eta, epochs, temp2(1:80,3:4), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(petalLearned2)
-petalwnorm2 = wnormalize(petalLearned2);
+% petalwnorm2 = wnormalize(petalLearned2);
+petalwnorm2 = norm_sigma(petalLearned2);
 printf("Normalized weight vector: ")
 disp(petalwnorm2)
 printf("Figure 5\n")
@@ -100,12 +110,13 @@ PlotIrisData(petalwnorm2, temp2(:,3:4), yValues)
 printf("---------------------------\n")
 printf("First & Third Petal -------------\n")
 % petalLearned3 = RunPLA(epochs, temp3(:,3:4), yValues);
-% petalLearned3 = RunDLA_ident(eta, epochs, temp3(:,3:4), yValues);
-% petalLearned3 = RunDLA_sigmoid(eta, epochs, temp3(:,3:4), yValues);
-petalLearned3 = RunDLA_tanh(eta, epochs, temp3(:,3:4), yValues);
+% petalLearned3 = RunDLA_ident(eta, epochs, temp3(1:80,3:4), yValues(1:80,1));
+% petalLearned3 = RunDLA_sigmoid(eta, epochs, temp3(1:80,3:4), yValues(1:80,1));
+petalLearned3 = RunDLA_tanh(eta, epochs, temp3(1:80,3:4), yValues(1:80,1));
 printf("Learned weight vector: ")
 disp(petalLearned3)
-petalwnorm3 = wnormalize(petalLearned3);
+% petalwnorm3 = wnormalize(petalLearned3);
+petalwnorm3 = norm_sigma(petalLearned3);
 printf("Normalized weight vector: ")
 disp(petalwnorm3)
 printf("Figure 6\n")
